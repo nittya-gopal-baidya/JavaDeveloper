5ca65f9ffb3d976868441ef79cf95a132fc51f59
Nittya Gopal Baidya <mrnittya@gmail.com>
2026-03-28 19:13:59 +0530

    Intial Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld{
     public static void main(String[]args){
-        System.out.println(("Hello World"));
+        System.out.println(("Hello World, Welcome and glad to have you with this Java Developer Journey"));
     }
 }
